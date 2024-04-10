72a9ca8de324f9ab76ece21a3d3bd25313093d1e
tracer-ics2023 <tracer@njuics.org>
2024-04-10 15:35:59 +0800

    >  compile NEMU 211180082 夏一然 Linux xyr-virtual-machine 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:35:59 up 2 days, 22:16,  1 user,  load average: 0.05, 0.03, 0.01

--- nemu/test_code/test.c
+++ nemu/test_code/test.c
@@ -6,6 +6,8 @@ int main()
 {
     char *line_read = NULL;
     line_read = readline("(nemu) ");
-    printf("%s\n",line_read);
+    char *cmd = strtok(line_read, " ");
+
+    printf("%s\n",cmd);
     return 0;
 }
