000164274109e291fa7ca0e405ab46045959a339
tracer-ics2023 <tracer@njuics.org>
2024-04-26 22:05:30 +0800

    >  compile NEMU 211180082 夏一然 Linux xyr-virtual-machine 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:05:30 up 10:47,  1 user,  load average: 4.05, 3.18, 2.87

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -71,6 +71,6 @@ static int decode_exec(Decode *s) {
 }
 
 int isa_exec_once(Decode *s) {
-  s->isa.inst.val = inst_fetch(&s->snpc, 4);
+  s->isa.inst.val = inst_fetch(&s->snpc, 4);    // 这里为何固定 length 为 4 ?
   return decode_exec(s);
 }
