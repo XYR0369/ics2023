3d116fabd37718337ddf3894467ee5e0aed4295e
tracer-ics2023 <tracer@njuics.org>
2024-05-10 15:55:38 +0800

    >  compile NEMU 211180082 夏一然 Linux xyr-virtual-machine 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:55:38 up 1 day, 20:21,  1 user,  load average: 0.07, 0.05, 0.06

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -240,7 +240,7 @@ uint32_t eval(int p, int q){
       case TK_EQ: return val1 == val2;
       case TK_NEQ: return val1 != val2;
       case TK_AND: return (!val1) ? 0: (!(val2 == 0));   // 考虑短路运算
-      default: Log("Invalid expression!"); return 0;
+      default: Log("Invalid expression!"); assert(0);
     }
   }
 }
