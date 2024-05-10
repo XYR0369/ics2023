46161a627b5805a312821b91a4d96ab7bfd8b459
tracer-ics2023 <tracer@njuics.org>
2024-05-10 16:09:20 +0800

    >  compile NEMU 211180082 夏一然 Linux xyr-virtual-machine 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:09:20 up 1 day, 20:35,  1 user,  load average: 0.01, 0.07, 0.06

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -105,8 +105,8 @@ static bool make_token(char *e) {
         int substr_len = pmatch.rm_eo;
 
     // 字符匹配调试信息
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-            i, rules[i].regex, position, substr_len, substr_len, substr_start);
+        // Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+        //     i, rules[i].regex, position, substr_len, substr_len, substr_start);
 
         position += substr_len;
 
