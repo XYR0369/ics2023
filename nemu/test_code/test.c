e2a98aea6411653ab0e6a26c8a8031fbb0faa137
lingardx <xyr0369@gmail.com>
2024-05-10 23:06:38 +0800

    the end of pa1

--- nemu/test_code/test.c
+++ nemu/test_code/test.c
@@ -8,20 +8,21 @@
 
 #include <stdio.h>
 
-int main() {
-    int value = 10; // 初始值
-    int a = 20;
-    int b = 30;
-    printf("Value is initially: %d\n", value);
+// int main() {
+//     int value = 10; // 初始值
+//     int a = 20;
+//     int b = 30;
+//     printf("Value is initially: %d\n", value);
 
-    // 这里可能会发生错误，导致 value 被错误地修改
-    if (1) {
-        value = value + 5; // 故意写错的地方，可能是 value = value - 5;
-        a = a+1;
-        b = b+1;
-    }
+//     // 这里可能会发生错误，导致 value 被错误地修改
+//     if (1) {
+//         value = value + 5; // 故意写错的地方，可能是 value = value - 5;
+//         a = a+1;
+//         b = b+1;
+//     }
 
-    printf("Value should be 15, but it is: %d\n", value);
+//     printf("Value should be 15, but it is: %d\n", value);
 
-    return 0;
-}
+//     return 0;
+
+
