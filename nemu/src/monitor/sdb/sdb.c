f8f7770e5a1af1e9799093ec6f0efe830b90b5a0
tracer-ics2023 <tracer@njuics.org>
2024-05-10 16:23:18 +0800

    >  compile NEMU 211180082 夏一然 Linux xyr-virtual-machine 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:23:18 up 1 day, 20:49,  1 user,  load average: 0.05, 0.03, 0.02

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -113,7 +113,7 @@ static int cmd_x(char* args){
     return 0;
   }
   printf("Result of expr: %u\n",addr);
-  printf("Content of mem: %u\n",addr);
+  printf("Content of mem: \n");
   // vaddr_t addr = strtol(arg, NULL, 16);   // 16 进制转换，简单验证正确性
   for(int i = 0; i < n; i++){
     printf("0x%x: 0x%08x\n", addr, vaddr_read(addr, 4));    
